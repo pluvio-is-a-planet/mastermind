a5887b0bcf6aaafa6038c785761f4051ed9658d1
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-11 16:59:05 +0200

    feat: add select_mode method
    Added the select_mode method to allow to player to choose between
    taking on the role of 'code maker' or 'code breaker', or to quit the
    game.

--- main/game.rb
+++ main/game.rb
@@ -6,4 +6,13 @@ class Game
   def start_game
     puts instructions
   end
+
+  def select_mode
+    result = gets.chomp
+    return result if result.match(/^[1-2]$/)
+    return result if %w[q exit].include?(result)
+
+    puts "Invalid input, please only enter '1', '2', 'q', or 'exit'"
+    select_mode
+  end
 end
