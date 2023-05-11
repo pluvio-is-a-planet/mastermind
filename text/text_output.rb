82076419341f629e3a568f4e766ef75adea77a25
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-11 16:24:44 +0200

    refactor: change all text colors to white

--- text/text_output.rb
+++ text/text_output.rb
@@ -11,10 +11,10 @@ module TextOutput
     {
       '1' => stylize_output('  1  ', 'color', '41;97'), # red bg with bright white text
       '2' => stylize_output('  2  ', 'color', '42;97'), # green bg with bright white text
-      '3' => stylize_output('  3  ', 'color', '43;90'), # yellow bg with bright black text
+      '3' => stylize_output('  3  ', 'color', '43;97'), # yellow bg with bright white text
       '4' => stylize_output('  4  ', 'color', '44;97'), # blue bg with bright white text
-      '5' => stylize_output('  5  ', 'color', '45;90'), # magenta bg with bright black text
-      '6' => stylize_output('  6  ', 'color', '46;90'), # cyan bg with bright black text
+      '5' => stylize_output('  5  ', 'color', '45;97'), # magenta bg with bright white text
+      '6' => stylize_output('  6  ', 'color', '46;97'), # cyan bg with bright white text
     }[num]
   end
 
