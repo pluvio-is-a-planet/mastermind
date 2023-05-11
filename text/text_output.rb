a5376075158dd74f3a04d239cca7fbd57f9dac72
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-11 15:52:17 +0200

    docs: add comments to describe styling

--- text/text_output.rb
+++ text/text_output.rb
@@ -3,18 +3,18 @@ module TextOutput
     {
       'underline' => "\e[4m#{text}\e[0m",
       'bright' => "\e[1m#{text}\e[0m",
-      'color' => "\e[#{color};1m#{text}\e[0m"
+      'color' => "\e[#{color}m#{text}\e[0m"
     }[style]
   end
 
   def code_color(num)
     {
-      '1' => stylize_output('  1  ', 'color', '41;37'),
-      '2' => stylize_output('  2  ', 'color', '42;37'),
-      '3' => stylize_output('  3  ', 'color', '43;30'),
-      '4' => stylize_output('  4  ', 'color', '44;37'),
-      '5' => stylize_output('  5  ', 'color', '45;30'),
-      '6' => stylize_output('  6  ', 'color', '46;30'),
+      '1' => stylize_output('  1  ', 'color', '41;97'), # red bg with bright white text
+      '2' => stylize_output('  2  ', 'color', '42;97'), # green bg with bright white text
+      '3' => stylize_output('  3  ', 'color', '43;90'), # yellow bg with bright black text
+      '4' => stylize_output('  4  ', 'color', '44;97'), # blue bg with bright white text
+      '5' => stylize_output('  5  ', 'color', '45;90'), # magenta bg with bright black text
+      '6' => stylize_output('  6  ', 'color', '46;90'), # cyan bg with bright black text
     }[num]
   end
 end
