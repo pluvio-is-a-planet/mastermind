86aad14f3d03e5919bb7ff0261cb77330f7b43d9
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-11 17:10:27 +0200

    feat: add random_key method
    Added the random_key method that generates a random key that the player
    has to then, as the code breaker, solve.

--- main/player.rb
+++ main/player.rb
@@ -2,4 +2,16 @@ require_relative 'game_logic'
 require_relative 'display'
 require_relative 'text_output'
 
-class Player; end
+class Player
+  # include Display
+  include GameLogic
+  include TextOutput
+
+  def initialize
+    @key = random_key
+  end
+
+  def random_key
+    result = (1..4).map { OPTIONS.sample }
+  end
+end
