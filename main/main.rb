2d9dd481cf8a437ce1b238c26326642d7703367d
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-11 18:13:51 +0200

    feat: link all files to main.rb file

---
+++ main/main.rb
@@ -0,0 +1,9 @@
+require_relative 'cpu'
+require_relative 'display'
+require_relative 'game_logic'
+require_relative 'game'
+require_relative 'player'
+require_relative '../text/text_instructions'
+require_relative '../text/text_output'
+
+Game.new.start_game
