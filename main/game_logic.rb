3545ea5afcb5877135ec04fede6c57a5d8d0cff4
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-11 16:37:57 +0200

    feat: add exact_match method
    Added the exact_match method that counts all of the elements in the key
    array, that are equal to an element in the guess array, with the same
    index.

--- main/game_logic.rb
+++ main/game_logic.rb
@@ -1,7 +1,17 @@
 module GameLogic
   def evaluate_guess(key, guess); end
 
-  def exact_match(key, guess); end
+  def exact_match(key, guess)
+    result = 0
+    key.each_with_index do |color, index|
+      next unless color == guess[index]
+
+      result += 1
+      key[index] = '*'
+      guess[index] = '*'
+    end
+    result
+  end
 
   def half_match(key, guess); end
 
