4be0c06e55aefd5aa4b2be475c82ef1ee65492a6
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-11 21:06:11 +0200

    refactor: separate messages method
    Separated messages method into turn_message, game_message, and
    error_message. This makes it easier to manage which message to display.

--- text/text_output.rb
+++ text/text_output.rb
@@ -1,18 +1,39 @@
 require_relative '../main/display'
 
+# rubocop:disable Layout/LineLength
+
+# module to display text for the game
 module TextOutput
   include Display
 
-  def messages(message, guesses = nil)
+  def turn_message(type, num = nil)
     {
-      'input error' => "Invalid input, please remember the rules above. (Enter 'q' or 'exit' to quit the game)>>",
-      'user wins' => "Congratulations! You have cracked the code after #{guesses} guesses.\n",
-      'user loses' => "Game over...\nThe code was:  ",
-      'cpu think' => 'Thinking...',
-      'turn' => "Turn \##{guesses} enter your guess>>",
-      'cpu wins' => "The computer cracked your code after #{guesses} guesses. Better luck next time.",
-      'play again prompt' => 'Would you like to play again?>>',
+      'prompt guess' => "Turn ##{num}: Enter your guess, or 'q' or 'exit' to quit the game.>>",
+      'computer turn' => "\nComputer turn: ##{num}>>",
+      'start guessing' => 'The secret key has been set, can you guess it?>>',
+      'prompt key' => 'Enter the code for the computer to guess>>',
+      'computer guess' => "is your key.\n"
+    }[type]
+  end
+
+  def game_message(type)
+    {
+      'user won' => "  Congratulations! You broke the key! \n\n",
+      'user lost' => 'Game over... The key was:',
+      'cpu won' => "\nGame over... The computer somehow managed to crack your key.",
+      'cpu lost' => "\nThe computer just could not crack your key and you won the game!",
+      'play again' => "\n\nWould you like to play again? Enter 'y' to play again or any other key to quit.",
       'thanks' => "Thank you so much for playing my version of #{stylize_output('Mastermind', 'color', '92')}!\nCheck out my Github at #{stylize_output('https://github.com/pluvio-is-a-planet', 'bright')}."
-    }[message]
+    }[type]
+  end
+
+  def error_message(type)
+    {
+      'mode' => stylize_output("Enter '1' to be the code MAKER or '2' to be the code BREAKER, or 'q' or 'exit' to quit.", 'color', '91'),
+      'turn' => stylize_output('Your guess should only contain 4 digits between 1 and 6.', 'color', '91'),
+      'key' => stylize_output('Your key has to be 4 characters long, using numbers between 1 and 6.', 'color', '91'),
+    }[type]
   end
 end
+
+# rubocop:enable Layout/LineLength
