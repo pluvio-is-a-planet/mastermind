a9f445f819499b1e1f54c4d0c70444281f8ea514
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-11 17:16:56 +0200

    feat: add player_guess method
    Added the player_guess method to allow the user to either enter their
    guess or, to enter 'q' or 'exit' to quit the game.

--- main/player.rb
+++ main/player.rb
@@ -16,4 +16,13 @@ class Player
   def random_key
     result = (1..4).map { OPTIONS.sample }
   end
+
+  def player_guess
+    result = gets.chomp
+    return result if result.match(/^[1-6]{4}$/)
+    return result if %[q exit].include?(result.downcase)
+
+    puts 'Invalid input, remember the rules!'
+    player_guess
+  end
 end
