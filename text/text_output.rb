4cdbc7ed88317eef8f2905201fcb6e118b6bb51f
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-11 15:27:55 +0200

    feat: add code_color method
    Added the code_color method that applies a different color to each
    number in the range 1-6.

--- text/text_output.rb
+++ text/text_output.rb
@@ -6,4 +6,15 @@ module TextOutput
       'color' => "\e[#{color};1m#{text}\e[m"
     }[style]
   end
+
+  def code_color(num)
+    {
+      '1' => stylize_output('  1  ', 'color', '41;37'),
+      '2' => stylize_output('  2  ', 'color', '42;37'),
+      '3' => stylize_output('  3  ', 'color', '43;30'),
+      '4' => stylize_output('  4  ', 'color', '44;37'),
+      '5' => stylize_output('  5  ', 'color', '45;30'),
+      '6' => stylize_output('  6  ', 'color', '46;30'),
+    }[num]
+  end
 end
