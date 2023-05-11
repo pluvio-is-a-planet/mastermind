c11dcf229bec19240f1023a4f71b8950bc5c1665
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-11 16:42:22 +0200

    feat: add half_match method
    Added the half_match method that counts all of the elements in the
    guess array, that exist in the key array, but do not share an index
    with an element of the same value.

--- main/game_logic.rb
+++ main/game_logic.rb
@@ -13,7 +13,18 @@ module GameLogic
     result
   end
 
-  def half_match(key, guess); end
+  def half_match(key, guess)
+    result = 0
+    guess.each_with_index do |color, index|
+      next unless color != key[index] && key.include? color
+
+      result += 1
+      to_be_removed = key.find_index(guess[index])
+      key[to_be_removed] = '?'
+      guess[index] = '?'
+    end
+    result
+  end
 
   def solved?(key, guess); end
 end
